97639024ac800df6d70654dbe2a3278cebc9d049
KingaMendyk <s25275@pjwstk.edu.pl>
2024-05-05 17:12:40 +0200

    Added player movement

--- UWP Sokoban/Assets/Scripts/Player/PlayerMovement.cs
+++ UWP Sokoban/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,18 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerMovement : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class PlayerMovement : MonoBehaviour, PlayerControls.IPlayerMoveActions {
+    [SerializeField] private float moveSpeed = 2f;
+    
+    private PlayerControls playerControls;
+    private Vector2 movementVector;
+
+    private void Awake() {
+        playerControls = new PlayerControls();
+        playerControls.PlayerMove.SetCallbacks(this);
+        playerControls.PlayerMove.Enable();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        movementVector.Normalize(); ;
+        transform.Translate(movementVector * (moveSpeed * Time.deltaTime));
+    }
+
+    public void OnMovement(InputAction.CallbackContext context) {
+        movementVector = context.ReadValue<Vector2>();
     }
 }
